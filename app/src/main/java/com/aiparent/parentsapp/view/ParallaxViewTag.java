3b360112a2193e1429759caac9522fa11bb6273e
weilanhzuan <471720348@qq.com>
2015-03-21 12:26:18 +0800

    初始化客户端

---
+++ app/src/main/java/com/aiparent/parentsapp/view/ParallaxViewTag.java
@@ -0,0 +1,11 @@
+package com.aiparent.parentsapp.view;
+
+public class ParallaxViewTag {
+  protected int index;
+  protected float xIn;
+  protected float xOut;
+  protected float yIn;
+  protected float yOut;
+  protected float alphaIn;
+  protected float alphaOut;
+}
